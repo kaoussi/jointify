961c65e606ea62e0c837025ca9346e640f65e71d
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-11-15 16:30:43 +0100

    Manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'jointify-artisoft',
+        short_name: 'Jointify',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#00eece',
+        theme_color: '#00eece',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
