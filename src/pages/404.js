2c36993d22c559dea39fa230a406a5d7d37ead63
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-11-15 16:17:47 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,17 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <main>
+    <section className="hero text-light text-center">
+        <div className="container-sm">
+          <div className="hero-inner">
+            <h1 className="hero-title h2-mobile mt-0 is-revealing">NOT FOUND.</h1>
+            <p className="hero-paragraph is-revealing">You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <p className="hero-cta is-revealing"><a className="button button-secondary button-shadow" href="/">Go back Home!</a></p>
+          </div>
+        </div>
+      </section>
+    </main>
   </Layout>
 )
 
