e3a6f351a8001c7da4cd0a129bd87c3494d0576b
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-11-16 04:47:49 +0100

    Quick weird fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 import Features from '../components/Features'
 import MentorsSection from '../components/MentorsSection'
 import Pricing from '../components/Pricing'
-import CtaSection from '../components/CtaSection'
+import CtaSection from '../components/CtaFormSection'
 import Conversation from '../../static/svg/conversation.svg'
 
 const SvgImage = styled(Conversation)`
